30e021924d4ca12e2d71ea64356ab9523a3e04d4
Neo_ZH <luckyyafei@163.com>
2018-10-09 09:54:39 +0800

    新建工程

---
+++ Transition_ObjC/Transition_ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Transition_ObjC
+//
+//  Created by Egeo 迩格 on 2018/10/9.
+//  Copyright © 2018 Neo_ZH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Transition_ObjC/Transition_ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Transition_ObjC
+//
+//  Created by Egeo 迩格 on 2018/10/9.
+//  Copyright © 2018 Neo_ZH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
